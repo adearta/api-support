1b941ceb6892e3f21358c0afebfba85ba846eae4
adeartakusuma <adearta48@gmail.com>
2021-05-25 12:30:13 +0700

    adding the end point desc

--- routes/api/webinar.php
+++ routes/api/webinar.php
@@ -8,18 +8,47 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('admin')->group(function () {
-    //create new webinar
+    //super admin create new webinar
+    //method post = > 
+    /* the parameter used:
+            1 -> zoom_link
+            2 -> event_name
+            3 -> event_date
+            4 -> event_time
+            5.-> event_picture
+            6 -> school[]
+            */
     Route::post('/create', [WebinarAkbarController::class, 'addWebinar']); // 1 ok
 });
+//method get = > 
+/* the parameter used:
+            1 -> {id} => school_id
+            */
 //showing list of webinar which can be joined when invitation status of the webinar is not 3(acc) and (2)
 Route::get('/list/{id}', [WebinarAkbarController::class, 'getWebinarBySchoolId']); // 3ok
-//get list of webinar can joined by the school id
-Route::get('/detail/{id}', [WebinarAkbarController::class, 'detailWebinar']); //7,5ok
+//get the detail of the webinar with schools that participate on it
+//method get = > 
+/* the parameter used:
+            1 -> {webinar_id} => webinar_id
+            */
+Route::get('/detail/{webinar_id}', [WebinarAkbarController::class, 'detailWebinar']);
 
 Route::group(['prefix' => 'school'], function () {
-    //add school participants by the admin when the school candidate not give student candidate until maximum date (3 days) 
+    //add school participants by the admin when the school candidate not give student acandidate and not confirm until maximum date (3 days) 
+    //method post = > 
+    /* the parameter used:
+            1 -> school_id
+            2 -> webinar_id
+            */
     Route::post('/add', [WebinarAkbarController::class, 'addSchoolParticipants'])->middleware('admin'); // 5
     //updatiing status of the school participants
+    //method post = > 
+    /* the parameter used:
+            1 -> webinar_id
+            2 -> school_id
+            3 -> start_year ->use this param when school_id = x has set status to 4(submit the data of student) 
+            4 -> status
+            */
     /* the status of school
             1 -> created
             2 -> rejected
@@ -30,15 +59,27 @@ Route::group(['prefix' => 'school'], function () {
     Route::post('/update-status', [SchoolParticipantAkbarController::class, 'updateSchoolWebinar']); // 6
 });
 
+
 Route::group(['prefix' => 'notification'], function () {
-    //get the notification 
+    //get the notification
+    //method get = >  
+    /* the parameter used:
+        on params 
+            1 -> school_id
+        on headers
+            1 -> accept-language =>set to (id/en)
+            */
     Route::get('/', [NotificationWebinarController::class, 'getNotification']); //2
     //set notification status to read 
+    //method get = >  
+    /* the parameter used:
+        on params 
+            1 -> notification_id
+            */
     Route::post('/read', [NotificationWebinarController::class, 'setNotificationReaded']); //extra
 });
 //super admin get the data from database (candidate name, and candidate school)
 Route::get('/participant/{webinar_id}', [WebinarAkbarController::class, 'participantList']);
-//get the detail of the webinar with schools that participate on it
-Route::get('/detail/{webinar_id}', [WebinarAkbarController::class, 'detailWebinar']);
+
 
 ////////////////////////////////////////////////////////////////////////
