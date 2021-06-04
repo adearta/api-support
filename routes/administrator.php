f0e346b052a5c84c23dc573b42b0ab1eca1c8746
adeartakusuma <adearta48@gmail.com>
2021-06-04 15:20:14 +0700

    fixing route, but need revision on some params

---
+++ routes/administrator.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\CertificateController;
+use App\Http\Controllers\NotificationNormalWebinarController;
+use App\Http\Controllers\WebinarAkbarController;
+use App\Http\Controllers\SchoolParticipantAkbarController;
+use App\Http\Controllers\NotificationWebinarController;
+use App\Http\Controllers\StudentNormalWebinarParticipantController;
+use App\Http\Controllers\WebinarNormalController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Payment\WebinarPaymentController;
+use App\Http\Controllers\WebinarOrderController;
+use App\Models\WebinarAkbarModel;
+
+Route::group(['prefix' => 'webinar-akbar'], function () {
+    Route::middleware('admin')->group(function () {
+        Route::post('/create', [WebinarAkbarController::class, 'addWebinar']);
+        Route::get('/list/{id}', [WebinarAkbarController::class, 'getWebinarBySchoolId']);
+        Route::post('/update-status', [SchoolParticipantAkbarController::class, 'updateSchoolWebinar']);
+        Route::get('/detail/{webinar_id}', [WebinarAkbarController::class, 'detailWebinar']);
+        Route::get('/participant/{webinar_id}', [WebinarAkbarController::class, 'participantList']);
+        Route::post('/detail/upload-certificate', [CertificateController::class, 'addCertificateAkbar']);
+        Route::group(['prefix' => 'notification'], function () {
+            Route::get('/', [NotificationWebinarController::class, 'getNotification']);
+            Route::post('/read', [NotificationWebinarController::class, 'setNotificationReaded']);
+        });
+    });
+});
+Route::group(['prefix' => 'webinar-internal'], function () {
+    Route::middleware('admin')->group(function () {
+        Route::get('/listwebinar', [WebinarNormalController::class, 'listNormalWebinar']);
+        Route::get('/detail/{webinar_id}', [WebinarNormalController::class, 'detailNormalWebinar']);
+        Route::get('/detail-list/student/{webinar_id}', [WebinarNormalController::class, 'detailNormalWebinarWithStudent']);
+        Route::get('/order/detail', [WebinarOrderController::class, 'getDetailOrder']);
+        Route::post('/create', [WebinarNormalController::class, 'addNormalWebinar']);
+        Route::post('/register', [StudentNormalWebinarParticipantController::class, 'registerStudent']); //ok
+        Route::post('/addcertificate', [CertificateController::class, 'addCertificate']);
+        Route::group(['prefix' => 'notification'], function () {
+            Route::get('/', [NotificationWebinarController::class, 'getNotification']);
+            Route::post('/read', [NotificationWebinarController::class, 'setNotificationReaded']);
+        });
+        Route::group(['prefix' => 'payment'], function () {
+            Route::get('/charge', [WebinarPaymentController::class, 'charge']);
+        });
+    });
+});
