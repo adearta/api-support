2115c8f4450dd01f252396da758f95ceaef83858
suastikaadinata <suastikaadinata97@gmail.com>
2021-06-20 20:38:30 +0800

    broadcast is done

--- app/Http/Controllers/BroadcastChat/BroadcastController.php
+++ app/Http/Controllers/BroadcastChat/BroadcastController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\BroadcastChat;
 
 use App\Models\BroadcastModel;
+use App\Models\NotificationModel;
 use App\Models\SchoolModel;
 use App\Models\StudentModel;
 use App\Models\UserEducationModel;
+use App\Models\UserPersonal;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Validator;
@@ -23,6 +25,8 @@ class BroadcastController extends Controller
     private $tbStudent;
     private $tbUserEducation;
     private $tbBroadcast;
+    private $tbNotification;
+    private $tbUser;
 
     public function __construct()
     {
@@ -30,6 +34,8 @@ class BroadcastController extends Controller
         $this->tbStudent = StudentModel::tableName();
         $this->tbUserEducation = UserEducationModel::tableName();
         $this->tbBroadcast = BroadcastModel::tableName();
+        $this->tbNotification = NotificationModel::tableName();
+        $this->tbUser = UserPersonal::tableName();
     }
 
     public function create(Request $request)
@@ -71,11 +77,11 @@ class BroadcastController extends Controller
                         'year'              => $request->year,
                     ));
 
-                switch ($request->broadcast_type) {
+                switch ($request->type) {
                     case 1:
                         $student_list = DB::connection('pgsql2')->table($this->tbStudent)
                             ->where('school_id', '=', $request->school_id)
-                            ->select('user_id as student_id')
+                            ->select('user_id')
                             ->get();
                         break;
                     case 2:
@@ -83,52 +89,62 @@ class BroadcastController extends Controller
                             ->leftJoin($this->tbUserEducation . ' as education', 'student.nim', '=', 'education.nim')
                             ->where('education.school_id', '=', $request->school_id)
                             ->where('education.start_year', '=', $request->year)
-                            ->select('student.user_id as student_id')
+                            ->select('student.user_id')
                             ->get();
                         break;
                     case 3:
-                        $studentTemp = [];
+                        $studentTemp = "";
                         $index = 0;
                         foreach ($request->student_id as $student) {
-                            $studentTemp[$index] = $student;
+                            if ($index == 0) {
+                                $studentTemp .= $student;
+                            } else {
+                                $student .= ", " . $student;
+                            }
                             $index++;
                         }
 
                         $student_list = DB::connection('pgsql2')->table($this->tbStudent)
-                            ->whereRaw('id = ANY(' . $studentTemp . ')')
-                            ->select('user_id as student_id')
+                            ->whereRaw('id = ANY(ARRAY[' . $studentTemp . '])')
+                            ->select('user_id')
                             ->get();
                         break;
                 }
 
                 foreach ($student_list as $student) {
                     //send notif to frisidea table
-                    DB::table($this->tbChat)->insert(array(
-                        'room_chat_id'      => $broadcast_id,
-                        'room_broadcast_id' => $broadcast_id,
-                        'chat'              => $request->chat,
-                        'image'             => $path,
-                        'link'              => $request->link,
-                        'type'              => 'broadcast',
-                        'sender'            => 'school',
-                        'send_time'         => $request->send_time,
+                    DB::connection('pgsql2')->table($this->tbNotification)->insert(array(
+                        'title'             => "New Announcement",
+                        'description'       => "You got a new announcement from school",
+                        'datetime'          => $request->send_time,
+                        'is_seen'           => false,
+                        'type'              => 5,
+                        'type_profile'      => 1,
+                        'type_education'    => 1,
+                        'user_id'           => $student->user_id,
+                        'title_en'          => "New Announcement",
+                        'title_ind'         => "Pengumuman Baru",
+                        'description_en'    => "You got a new announcement from school",
+                        'description_ind'   => "Anda mendapatkan pengumuman baru dari sekolah",
+                        'updated'           => Carbon::now(),
+                        'broadcast_id'      => $broadcast_id
                     ));
                 }
 
-                //get broadcast from db by broadcast_id
                 $broadcast = BroadcastModel::find($broadcast_id);
                 $response_path = null;
-                if ($broadcast[0]->image != null) {
-                    $response_path = env("WEBINAR_URL") . $broadcast[0]->image;
+                if ($broadcast->image != null) {
+                    $response_path = env("WEBINAR_URL") . $broadcast->image;
                 }
 
                 $broadcastResponse = (object) array(
-                    'id'                => $broadcast[0]->id,
-                    'chat'              => $broadcast[0]->chat,
+                    'id'                => $broadcast->id,
+                    'chat'              => $broadcast->chat,
                     'image'             => $response_path,
-                    'link'              => $broadcast[0]->link,
-                    'year'              => $broadcast[0]->year,
-                    'send_time'         => $broadcast[0]->send_time,
+                    'link'              => $broadcast->link,
+                    'type'              => $broadcast->type,
+                    'year'              => $broadcast->year,
+                    'send_time'         => $broadcast->send_time,
                     'total_student'     => count($student_list)
                 );
 
@@ -143,7 +159,7 @@ class BroadcastController extends Controller
         }
     }
 
-    public function listRoomBroadcast(Request $request)
+    public function list(Request $request)
     {
         /*
         Param:
@@ -153,7 +169,7 @@ class BroadcastController extends Controller
         */
         $validation = Validator::make($request->all(), [
             'school_id' => 'required|numeric|exists:pgsql2.' . $this->tbSchool . ',id',
-            'page'      => 'numeric',
+            'page'      => 'required|numeric',
         ]);
 
         if ($validation->fails()) {
@@ -162,55 +178,61 @@ class BroadcastController extends Controller
             $status = DB::transaction(function () use ($request) {
                 $current_page = 1;
                 $data = [];
-                $query_pagination = "";
-                $query_search = "";
                 $start_item = 0;
 
-                $room_count = DB::select('select count(id) from ' . $this->tbRoomBroadcast . ' where school_id = ' . $request->school_id);
-                $total_page = ceil($room_count[0]->count / 10);
+                $broadcast_count = DB::table($this->tbBroadcast)
+                    ->selectRaw('count(id)')
+                    ->where('school_id', '=', $request->school_id)
+                    ->get();
+                $total_page = ceil($broadcast_count[0]->count / 10);
 
-                if ($request->page != null && $request->page > 1) {
+                if ($request->page != null && $request->page > 0) {
                     $current_page = $request->page;
 
                     if ($current_page > 1) {
                         $start_item = ($current_page - 1) * 10;
                     }
+                } else {
+                    $current_page = 0;
                 }
 
-                if ($current_page <= $total_page) {
-                    $query_pagination = " limit 10 offset " . $start_item;
-
+                if ($current_page > 0 && $current_page <= $total_page) {
+                    $search = "";
                     if ($request->search != null) {
                         $searchLength = preg_replace('/\s+/', '', $request->search);
                         if (strlen($searchLength) > 0) {
                             $search = strtolower($request->search);
-                            $query_search = " and lower(chat) like '%" . $search . "%'";
                         }
                     }
 
-                    $join = 'left join ' . $this->tbChat . ' as broadcast on room.id = broadcast.room_broadcast_id';
-                    $room = DB::select('select distinct on (broadcast.room_broadcast_id) room.id, room.broadcast_type, broadcast.id as chat_id, broadcast.chat, broadcast.image, broadcast.link, room.year from ' . $this->tbRoomBroadcast . ' as room ' . $join . ' where room.school_id = ' . $request->school_id . $query_search . " order by broadcast.room_broadcast_id desc" . $query_pagination);
+                    $broadcast = DB::table($this->tbBroadcast)
+                        ->where('school_id', '=', $request->school_id)
+                        ->whereRaw("lower(chat) like '%" . $search . "%'")
+                        ->orderBy('id', 'desc')
+                        ->offset($start_item)
+                        ->limit(10)
+                        ->get();
 
-                    for ($i = 0; $i < count($room); $i++) {
+                    for ($i = 0; $i < count($broadcast); $i++) {
                         $response_path = null;
-                        if ($room[$i]->image != null) {
-                            $response_path = env("WEBINAR_URL") . $room[$i]->image;
+                        if ($broadcast[$i]->image != null) {
+                            $response_path = env("WEBINAR_URL") . $broadcast[$i]->image;
                         }
 
-                        $broadcast = DB::table($this->tbChat)
+                        $broadcastCount = DB::connection('pgsql2')->table($this->tbNotification)
                             ->selectRaw('count(id)')
-                            ->where('room_broadcast_id', '=', $room[$i]->id)
+                            ->where('broadcast_id', '=', $broadcast[$i]->id)
                             ->get();
 
                         $data[$i] = (object) array(
-                            'room_broadcast_id' => $room[0]->id,
-                            'chat_id'           => $room[0]->chat_id,
-                            'chat'              => $room[0]->chat,
+                            'id'                => $broadcast[$i]->id,
+                            'chat'              => $broadcast[$i]->chat,
                             'image'             => $response_path,
-                            'link'              => $room[0]->link,
-                            'year'              => $room[0]->year,
-                            'broadcast_type'    => $room[0]->broadcast_type,
-                            'total_student'     => $broadcast[0]->count
+                            'link'              => $broadcast[$i]->link,
+                            'type'              => $broadcast[$i]->type,
+                            'year'              => $broadcast[$i]->year,
+                            'send_time'         => $broadcast[$i]->send_time,
+                            'total_student'     => $broadcastCount[0]->count
                         );
                     }
                 }
@@ -222,7 +244,7 @@ class BroadcastController extends Controller
                         'last_page'     => $total_page,
                         'current_page'  => $current_page,
                         'current_data'  => count($data), // total data based on filter search and page
-                        'total_data'    => $room_count[0]->count
+                        'total_data'    => $broadcast_count[0]->count
                     )
                 );
                 return $response;
@@ -236,27 +258,27 @@ class BroadcastController extends Controller
         }
     }
 
-    public function delete($room_id)
+    public function delete($broadcast_id)
     {
-        $validation = Validator::make(['room_id' => $room_id], [
-            'room_id' => 'required|numeric|exists:' . $this->tbRoomBroadcast . ',id'
+        $validation = Validator::make(['broadcast_id' => $broadcast_id], [
+            'broadcast_id' => 'required|numeric|exists:' . $this->tbBroadcast . ',id'
         ]);
         if ($validation->fails()) {
             return $this->makeJSONResponse($validation->errors(), 400);
         } else {
-            $data = DB::transaction(function () use ($room_id) {
-                $broadcast = DB::select('select distinct on (room_broadcast_id) image from ' . $this->tbChat . ' where room_broadcast_id = ' . $room_id);
+            $data = DB::transaction(function () use ($broadcast_id) {
+                $broadcast = BroadcastModel::find($broadcast_id);
 
-                if ($broadcast[0]->image != null) {
-                    if (Storage::disk('public')->exists($broadcast[0]->image)) {
-                        Storage::disk('public')->delete($broadcast[0]->image);
+                if ($broadcast->image != null) {
+                    if (Storage::disk('public')->exists($broadcast->image)) {
+                        Storage::disk('public')->delete($broadcast->image);
                     }
                 }
 
-                DB::table($this->tbRoomBroadcast)
-                    ->where('id', '=', $room_id)
+                DB::connection('pgsql2')->table($this->tbNotification)
+                    ->where('broadcast_id', '=', $broadcast->id)
                     ->delete();
-
+                $broadcast->delete();
                 return true;
             });
 
@@ -274,10 +296,11 @@ class BroadcastController extends Controller
         Param:
         1. Room id
         2. Page -> default(0 or null)
+        3. Search -> by student name
         */
         $validation = Validator::make($request->all(), [
-            'room_id'   => 'required|numeric|exists:' . $this->tbRoomBroadcast . ',id',
-            'page'      => 'numeric',
+            'broadcast_id'      => 'required|numeric|exists:' . $this->tbBroadcast . ',id',
+            'page'              => 'required|numeric',
         ]);
 
         if ($validation->fails()) {
@@ -287,42 +310,43 @@ class BroadcastController extends Controller
                 $current_page = 1;
                 $student = [];
                 $start_item = 0;
+                $search = "";
 
-                $broadcast = DB::table($this->tbChat, 'chat')
-                    ->leftJoin($this->tbRoomBroadcast . ' as broadcast', 'chat.room_broadcast_id', '=', 'broadcast.id')
-                    ->leftJoin($this->tbRoomChat . ' as room', 'chat.room_id', '=', 'room.id')
-                    ->where('chat.room_broadcast_id', '=', $request->room_id)
-                    ->select('broadcast.id', 'broadcast.school_id', 'broadcast.broadcast_type', 'broadcast.year', 'room.student_id', 'chat.id as chat_id', 'chat.chat', 'chat.image', 'chat.link')
-                    ->limit(1)
+                $broadcast = BroadcastModel::find($request->broadcast_id);
+                $notification = DB::connection('pgsql2')->table($this->tbNotification)
+                    ->where('broadcast_id', '=', $request->broadcast_id)
+                    ->select('id')
                     ->get();
 
-                $total_page = ceil(count($broadcast) / 10);
+                $total_page = ceil(count($notification) / 10);
 
-                if ($request->page != null && $request->page > 1) {
+                if ($request->page != null && $request->page > 0) {
                     $current_page = $request->page;
 
                     if ($current_page > 1) {
                         $start_item = ($current_page - 1) * 10;
                     }
+                } else {
+                    $current_page = 0;
                 }
 
-                if ($current_page <= $total_page) {
-                    $broadcastStudent = DB::table($this->tbChat, 'chat')
-                        ->leftJoin($this->tbRoomChat . ' as room', 'chat.room_id', '=', 'room.id')
-                        ->where('chat.room_broadcast_id', '=', $request->room_id)
-                        ->orderBy('room.id', 'asc')
+                if ($current_page > 0 && $current_page <= $total_page) {
+                    if ($request->search != null) {
+                        $searchLength = preg_replace('/\s+/', '', $request->search);
+                        if (strlen($searchLength) > 0) {
+                            $search = strtolower($request->search);
+                        }
+                    }
+
+                    $student = DB::connection('pgsql2')->table($this->tbNotification, 'notif')
+                        ->leftJoin($this->tbStudent . ' as student', 'notif.user_id', '=', 'student.user_id')
+                        ->leftJoin($this->tbUser . ' as personal', 'notif.user_id', '=', 'personal.id')
+                        ->where('notif.broadcast_id', '=', $broadcast->id)
+                        ->whereRaw("lower(concat(personal.first_name,' ',personal.last_name)) like '%" . $search . "%'")
+                        ->orderBy('personal.id', 'asc')
                         ->offset($start_item)
                         ->limit(10)
-                        ->select('room.student_id')
                         ->get();
-
-                    for ($i = 0; $i < count($broadcastStudent); $i++) {
-                        $getStudent = DB::connection('pgsql2')->table($this->tbStudent)
-                            ->where('id', '=', $broadcastStudent[$i]->student_id)
-                            ->get();
-
-                        $student[$i] = $getStudent[0];
-                    }
                 }
 
                 $listStudent = (object) array(
@@ -332,23 +356,22 @@ class BroadcastController extends Controller
                         'last_page'     => $total_page,
                         'current_page'  => $current_page,
                         'current_data'  => count($student), // total data based on filter search and page
-                        'total_data'    => count($broadcast)
+                        'total_data'    => count($notification)
                     )
                 );
 
                 $response_path = null;
-                if ($broadcast[0]->image != null) {
-                    $response_path = $broadcast[0]->image;
+                if ($broadcast->image != null) {
+                    $response_path = $broadcast->image;
                 }
 
                 return (object) array(
-                    'room_broadcast_id' => $broadcast[0]->id,
-                    'chat_id'           => $broadcast[0]->chat_id,
-                    'chat'              => $broadcast[0]->chat,
+                    'id' => $broadcast->id,
+                    'chat'              => $broadcast->chat,
                     'image'             => $response_path,
-                    'link'              => $broadcast[0]->link,
-                    'year'              => $broadcast[0]->year,
-                    'broadcast_type'    => $broadcast[0]->broadcast_type,
+                    'link'              => $broadcast->link,
+                    'type'              => $broadcast->type,
+                    'year'              => $broadcast->year,
                     'student'           => $listStudent
                 );
             });
